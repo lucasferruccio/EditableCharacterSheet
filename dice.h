312afefe6754b4f48b91ad452bf04eb1a0e6bfcc
Lucas Paiva <104527748+lucasferruccio@users.noreply.github.com>
2022-08-24 17:59:42 -0300

    Create dice.h

---
+++ dice.h
@@ -0,0 +1,17 @@
+int maindice(char argv[]);
+
+int checkdice(char dice[]);
+
+void roll_dice(int dice, int dice_quantity);
+
+void split_dice(char dice[]);
+
+int dice_quantity(char dice[], int split);
+
+int dice_value(char dice[], int split);
+
+int mod_calc(char dice[], int mod_place);
+
+void result(int modifier, int dice_quantity, int value);
+
+void calc_advdis(char dice[]);
